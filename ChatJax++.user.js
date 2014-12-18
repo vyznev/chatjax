39cc155b4a838765dc1902b89f0d69358b2ece95
Ilmari Karonen <nospam@vyznev.net>
2014-12-18 22:17:39 +0200

    v0.1.4: initial commit

---
+++ ChatJax++.user.js
@@ -0,0 +1,114 @@
+// ==UserScript==
+// @name        ChatJax++
+// @namespace   https://github.com/vyznev/
+// @description Enable MathJax in Stack Exchange chat
+// @author      Ilmari Karonen
+// @version     0.1.4
+// @copyright   2014, Ilmari Karonen (http://stackapps.com/users/10283/ilmari-karonen)
+// @license     ISC; http://opensource.org/licenses/ISC
+// @match       *://chat.stackexchange.com/*
+// @grant       none
+// ==/UserScript==
+
+
+// Copyright (C) 2014 Ilmari Karonen.
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+// In addition to the license granted above, I, Ilmari Karonen, to the extent I
+// am authorized to do so, and subject to the disclaimer stated above, hereby
+// grant Stack Exchange, Inc. permission to make use of this software in any
+// way they see fit, including but not limited to incorporating all or parts of
+// it within the Stack Exchange codebase, with or without credit to myself.
+// This permission grant does not extend to any code written by third parties,
+// unless said parties also agree to it.
+
+
+( function () {  // start of anonymous wrapper function (needed to restrict variable scope on Opera)
+
+// Opera does not support @match, so re-check that we're on SE chat before doing anything
+if ( location.hostname != 'chat.stackexchange.com' ) return;
+
+// TODO: dynamically load config and MathJax URL from main site
+var mathJaxURL = "//beta.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML-full";
+var config = {
+	"HTML-CSS": { preferredFont: "TeX", availableFonts: ["STIX","TeX"], linebreaks: { automatic:true }, EqnChunk: 50 },
+	tex2jax: { inlineMath: [ ["$", "$"], ["\\\\(","\\\\)"] ], displayMath: [ ["$$","$$"], ["\\[", "\\]"] ], processEscapes: true, ignoreClass: "tex2jax_ignore|dno" },
+	TeX: {  noUndefined: { attributes: { mathcolor: "red", mathbackground: "#FFEEEE", mathsize: "90%" } }, Macros: { href: "{}" } },
+	messageStyle: "none"
+};
+config.elements = ['main', 'content', 'starred-posts'];  // don't parse mathjax in sidebar, except for starred posts
+
+// Chat polling code:
+var chatJaxSetup = function () {
+	if ( window.SOUP ) {
+		// Great, we have SOUP!  Just use its chat monitoring feature:
+		SOUP.hookChat( function ( json ) {
+			try {
+				if ( !window.MathJax || !window.CHAT ) return;
+				var data = JSON.parse( json );
+				var room = data['r' + CHAT.CURRENT_ROOM_ID];
+				if ( !room || !room.e || !room.e.forEach ) return;
+
+				var seen = {};
+				room.e.forEach( function ( e ) {
+					console.log( 'MathJax++ got event', e );
+					var type = e.event_type;
+					if ( !type || seen[type] ) return;
+					seen[type] = true;
+
+					if ( type == 1 || type == 2 || type == 20 ) {
+						MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'message-' + e.message_id ] );
+					}
+					else if ( type == 6 ) setInterval( function () {
+						// XXX: for some reason, starred posts need an extra delay :(
+						MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'starred-posts'] );
+					}, 10 );
+				} );
+			} catch (err) {
+				console.log( "ChatJax++ event hook failed", err );
+			}
+		} );
+
+		// re-typeset content loaded via AJAX to avoid race conditions
+		SOUP.hookAjax( /^\/chats\/\d+\/events\b/, function () {
+			window.MathJax && MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'main' ] );
+		} );
+		SOUP.hookAjax( /^\/chats\/stars\/\d+\b/, function () {
+			window.MathJax && MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'starred-posts' ] );
+		}, 10 );
+		
+		// also catch expansion of collapsed posts
+		var urlRegexp = /^\/messages\/(\d+)\/(\d+)\b/;
+		SOUP.hookAjax( urlRegexp, function ( event, xhr, settings ) {
+			if ( !window.MathJax || !window.CHAT ) return;
+			var match = urlRegexp.exec( settings.url );
+			if ( !match || match[1] != CHAT.CURRENT_ROOM_ID ) return;
+			MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'message-' + match[2] ] );
+		} );
+	}
+	else console.log("ChatJax++: TODO: Implement non-SOUP functionality!");
+};
+
+// Inject MathJax config and chat polling code to page:
+var configScript = document.createElement( 'script' );
+configScript.type = 'text/x-mathjax-config';
+configScript.textContent = "MathJax.Hub.Config(" + JSON.stringify(config) + ");\n(" + chatJaxSetup + ")();";
+document.head.appendChild( configScript );
+
+// Load MathJax itself:
+var mathJaxScript = document.createElement( 'script' );
+mathJaxScript.src = mathJaxURL;
+document.head.appendChild( mathJaxScript );
+
+} )();  // end of anonymous wrapper function
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-chatjax
-=======
+ChatJax++
+=========
 
-ChatJax++, a user script to enable MathJax in Stack Exchange chat
+ChatJax++ is a user script to enable MathJax in Stack Exchange chat.  It aims to be an easier, faster and more lightweight alternative to [RobJohn's ChatJax bookmarklets](http://www.math.ucla.edu/~robjohn/math/mathjax.html).
+
+Note that this user script is still under development.  Some issues to be fixed before v1.0 include:
+
+* ChatJax++ currently requires [SOUP](https://github.com/vyznev/soup) in order to capture chat events.
+* ChatJax++ currently runs in all chat rooms, not just those associated with sites using MathJax.
+* There is currently no way to toggle MathJax processing on and off (except by disabling ChatJax++ in you user script manager and reloading the page).
