8b0701441afc61b65c5984ae21b4ec6df026f855
Ilmari Karonen <nospam@vyznev.net>
2017-04-13 04:23:57 +0300

    v0.2.0: automatically load mathjax config from parent site

--- ChatJax++.user.js
+++ ChatJax++.user.js
@@ -3,18 +3,22 @@
 // @namespace   https://github.com/vyznev/
 // @description Enable MathJax in Stack Exchange chat
 // @author      Ilmari Karonen
-// @version     0.1.8
-// @copyright   2014-2015, Ilmari Karonen (http://stackapps.com/users/10283/ilmari-karonen)
+// @version     0.2.0
+// @copyright   2014-2017, Ilmari Karonen (http://stackapps.com/users/10283/ilmari-karonen)
 // @license     ISC; http://opensource.org/licenses/ISC
 // @match       *://chat.stackexchange.com/*
 // @homepageURL https://github.com/vyznev/chatjax
 // @updateURL   https://github.com/vyznev/chatjax/raw/master/ChatJax%2B%2B.user.js
 // @downloadURL https://github.com/vyznev/chatjax/raw/master/ChatJax%2B%2B.user.js
-// @grant       none
+// @grant       GM_xmlhttpRequest
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @connect     stackexchange.com
+// @connect     mathoverflow.net
 // ==/UserScript==
 
 
-// Copyright (C) 2014 Ilmari Karonen.
+// Copyright (C) 2014-2017 Ilmari Karonen.
 //
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
@@ -37,69 +41,74 @@
 // unless said parties also agree to it.
 
 
-( function () {  // start of anonymous wrapper function (needed to restrict variable scope on Opera)
+( function () { try { // start of anonymous wrapper function (needed to restrict variable scope on Opera)
 
-// Opera does not support @match, so re-check that we're on SE chat before doing anything
+// Old Opera does not support @match, so re-check that we're on SE chat before doing anything
 if ( location.hostname != 'chat.stackexchange.com' ) return;
 
-// Baseline MathJax URL and config, copied from SE:
-var mathJaxURL = "//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML-full";
-var config = {
-	"HTML-CSS": { preferredFont: "TeX", availableFonts: ["STIX","TeX"], linebreaks: { automatic:true }, EqnChunk: 50 },
-	tex2jax: { inlineMath: [ ["$", "$"], ["\\\\(","\\\\)"] ], displayMath: [ ["$$","$$"], ["\\[", "\\]"] ], processEscapes: true, ignoreClass: "tex2jax_ignore|dno" },
-	TeX: { noUndefined: { attributes: { mathcolor: "red", mathbackground: "#FFEEEE", mathsize: "90%" } }, Macros: { href: "{}" } },
-	messageStyle: "none"
-};
-
-// List of MathJax enabled sites from http://meta.stackexchange.com/a/216607:
-var mathJaxSites = /^(https?:)?\/\/(meta\.)?((astronomy|aviation|biology|chemistry|codereview|cogsci|crypto|cs(theory)?|dsp|earthscience|electronics|engineering|ham|math(educators|ematica)?|physics|puzzling|quant|robotics|scicomp|space|stats|worldbuilding)\.stackexchange\.com|mathoverflow\.net)(\/|$)/;
-
-var footerLink = document.querySelector('#footer-logo a');
-if ( !footerLink ) return;
-var match = mathJaxSites.exec( footerLink.href );
-if ( !match ) return;
-
-// Some sites have special config options:
-if ( /^(biology|chemistry)\./.test( match[3] ) ) config.TeX.extensions = ['mhchem.js'];
-if ( /^(codereview|electronics)\./.test( match[3] ) ) config.tex2jax.inlineMath = [ ["\\$", "\\$"] ];
-
-
-// Chat polling code:
-var chatJaxSetup = function () {
-	if ( !window.MathJax || !window.CHAT ) return;
-	if ( !window.SOUP ) {
-		console.log("ChatJax++: TODO: Implement non-SOUP functionality!");
+// Loader code, to be run after MathJax config has been fetched from parent site:
+var injectChatJax = function ( siteName, siteConfig ) {
+	if ( !siteConfig.mathJaxURL || !siteConfig.mathJaxConfig ) {
+		console.log( 'ChatJax++: ' + siteName + ' does not seem to have MathJax enabled, stopping' );
 		return;
 	}
-	// Great, we have SOUP!  Just use its chat monitoring feature:
-	SOUP.hookChat( function ( json ) {
-		try {
-			var data = JSON.parse( json );
-			var room = data['r' + CHAT.CURRENT_ROOM_ID];
-			if ( !room || !room.e || !room.e.forEach ) return;			
-			var seen = {};
-			room.e.forEach( function ( e ) {
-				console.log( 'MathJax++ got event', e );
-				var id = null;
-				switch ( e.event_type ) {
-					case 1: case 2: case 20: id = 'message-' + e.message_id; break;
-					case 6: id = 'starred-posts'; break;
-					case 22: id = 'feed-ticker'; break;
-				}
-				if ( !id || seen[id] ) return;
-				seen[id] = true;
-				
-				if ( id != 'starred-posts' ) {
-					MathJax.Hub.Queue( ['Typeset', MathJax.Hub, id] );
-				} else setTimeout( function () {
-					// XXX: for some reason, starred posts need an extra delay :(
-					MathJax.Hub.Queue( ['Typeset', MathJax.Hub, id] );
-				}, 10 );
-			} );
-		} catch (err) {
-			console.log( "ChatJax++ event hook failed", err );
-		}
-	} );
+
+	// Inject MathJax config from parent site, append ChatJax setup:
+	var config = siteConfig.mathJaxConfig;
+	config.push( "(" + chatJaxSetup + ")()" );
+	for (var i = 0; i < config.length; i++) {
+		var configScript = document.createElement( 'script' );
+		configScript.type = 'text/x-mathjax-config';
+		configScript.textContent = config[i];
+		document.head.appendChild( configScript );
+	}
+	config.pop();  // avoid setup script showing up in console when examining config
+
+	// CSS hack to get rid of spurious scroll bars on Firefox/Win:
+	var styleHack = document.createElement( 'style' );
+	styleHack.type = 'text/css';
+	styleHack.textContent = "div.message .full, div.message .partial { padding-bottom: 2px }";
+	document.head.appendChild( styleHack );
+
+	// Actually load MathJax:
+	var loaderScript = document.createElement( 'script' );
+	loaderScript.src = siteConfig.mathJaxURL;
+	document.head.appendChild( loaderScript );
+};
+
+// Chat polling code, will be injected as a MathJax config script:
+var chatJaxSetup = function () {
+	if ( !window.CHAT ) return;
+	if ( !window.SOUP ) {
+		console.log("ChatJax++ TODO: Implement non-SOUP functionality!");
+		return;
+	}
+	SOUP.hookChat( function ( json ) { try {
+		var data = JSON.parse( json );
+		var room = data['r' + CHAT.CURRENT_ROOM_ID];
+		if ( !room || !room.e || !room.e.forEach ) return;
+		var seen = {};
+		room.e.forEach( function ( e ) {
+			var id = null;
+			switch ( e.event_type ) {
+				// http://paste.ubuntu.com/12785810/
+				case 1: case 2: case 20: id = 'message-' + e.message_id; break;
+				case 6: id = 'starred-posts'; break;
+				case 22: id = 'feed-ticker'; break;
+				case 3: case 4: case 8: case 25: case 30: case 34: return;  // ignore user events
+				// default: console.log( 'ChatJax++ got unrecognized event', e );
+			}
+			if ( !id || seen[id] ) return;
+			seen[id] = true;
+
+			if ( id != 'starred-posts' ) {
+				MathJax.Hub.Queue( ['Typeset', MathJax.Hub, id] );
+			} else setTimeout( function () {
+				// XXX: for some reason, starred posts need an extra delay :(
+				MathJax.Hub.Queue( ['Typeset', MathJax.Hub, id] );
+			}, 10 );
+		} );
+	} catch (e) { console.log( 'ChatJax++ error:', e ); } } );
 
 	// re-typeset content loaded via AJAX to avoid race conditions
 	SOUP.hookAjax( /^\/chats\/\d+\/events\b/, function () {
@@ -108,7 +117,10 @@ var chatJaxSetup = function () {
 	SOUP.hookAjax( /^\/chats\/stars\/\d+\b/, function () {
 		MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'starred-posts' ] );
 	}, 10 );
-		
+	SOUP.hookAjax( /^\/rooms\/thumbs\/\d+\b/, function () {
+		MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'roomdesc' ] );
+	}, 10 );
+
 	// also catch expansion of collapsed posts
 	var urlRegexp = /^\/messages\/(\d+)\/(\d+)\b/;
 	SOUP.hookAjax( urlRegexp, function ( event, xhr, settings ) {
@@ -116,29 +128,72 @@ var chatJaxSetup = function () {
 		if ( !match || match[1] != CHAT.CURRENT_ROOM_ID ) return;
 		MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'message-' + match[2] ] );
 	} );
-	
-	// don't parse MathJax in sidebar (except for starred posts)
+
+	// don't parse MathJax in sidebar (except for starred posts and room description)
 	var sidebar = document.getElementById('sidebar');
 	if (sidebar) sidebar.className += ' tex2jax_ignore';
-	
+
 	MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'starred-posts' ] );
+	MathJax.Hub.Queue( ['Typeset', MathJax.Hub, 'roomdesc' ] );
+	console.log( 'ChatJax++ setup complete' );
 };
 
-// CSS hack to get rid of spurious scroll bars on Firefox/Win:
-var styleHack = document.createElement( 'style' );
-styleHack.type = 'text/css';
-styleHack.textContent = "div.message .full, div.message .partial { padding-bottom: 2px }";
-document.head.appendChild( styleHack );
+// Extract parent site URL from footer:
+var footerLink = document.querySelector('#footer-logo a');
+if ( !footerLink ) return;
+var match = /^((?:https?:)?\/\/((?:[0-9A-Za-z\-]+\.)*(?:stackexchange\.com|mathoverflow\.net)))(\/|$)/.exec( footerLink.href );
+if ( !match ) return;
+var parentURL = match[1] + '/404', siteName = match[2];
 
-// Inject MathJax config and chat polling code to page:
-var configScript = document.createElement( 'script' );
-configScript.type = 'text/x-mathjax-config';
-configScript.textContent = "MathJax.Hub.Config(" + JSON.stringify(config) + ");\n(" + chatJaxSetup + ")();";
-document.head.appendChild( configScript );
+// Load cached MathJax config data, purge expired entries:
+var configCacheKey = 'MathJaxConfigCache';
+var configCache = JSON.parse( GM_getValue( configCacheKey ) || "{}" );
+var now = Date.now(), newConfigCache = {}, expired = false;
+for (var site in configCache) {
+	var siteConfig = configCache[site];
+	if (siteConfig.disabled || (siteConfig.timeStamp && siteConfig.timeStamp >= now - 60*60*1000)) {
+		newConfigCache[site] = siteConfig;
+	} else expired = true;
+}
+if (expired) {
+	configCache = newConfigCache;
+	GM_setValue( configCacheKey, JSON.stringify( configCache ) );
+}
 
-// Load MathJax itself:
-var mathJaxScript = document.createElement( 'script' );
-mathJaxScript.src = mathJaxURL;
-document.head.appendChild( mathJaxScript );
+// Check if MathJax config for parent site is cached, otherwise load it:
+if ( configCache[siteName] ) {
+	console.log( 'ChatJax++ using cached MathJax config for ' + siteName + ':', configCache[siteName] );
+	injectChatJax( siteName, configCache[siteName] );
+} else {
+	console.log( 'ChatJax++ retrieving MathJax config from', parentURL );
+	GM_xmlhttpRequest( {
+		method: "GET",
+		url: parentURL,
+		headers: { "Accept": "text/html" },
+		onload: function ( response ) { try {
+			var parser = new DOMParser();
+			var doc = parser.parseFromString( response.responseText, 'text/html' );
 
-} )();  // end of anonymous wrapper function
+			var mathJaxScript = doc.querySelector( 'script[src*="/MathJax.js"]' );
+			var configScripts = doc.querySelectorAll( 'script[type="text/x-mathjax-config"]' );
+
+			var siteConfig = { timeStamp: now };
+			if ( configScripts.length && mathJaxScript ) {
+				siteConfig.mathJaxURL = mathJaxScript.getAttribute('src');
+				siteConfig.mathJaxConfig = [];
+				for (var i = 0; i < configScripts.length; i++) {
+					siteConfig.mathJaxConfig.push( configScripts[i].textContent );
+				}
+			}
+
+			console.log( 'ChatJax++ caching MathJax config for ' + siteName + ':', siteConfig );
+			configCache[siteName] = siteConfig;
+			GM_setValue( configCacheKey, JSON.stringify( configCache ) );
+
+			injectChatJax( siteName, siteConfig );
+		} catch (e) { console.log( 'ChatJax++ error:', e ); } }
+	} );
+}
+
+} catch (e) { console.log( 'ChatJax++ error:', e ); } } )();  // end of anonymous wrapper function
+
